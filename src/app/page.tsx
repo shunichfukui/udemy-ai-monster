16301bd91e978fc7e14e3778119f8392cc616fa6
shunich <fukusyun3@gmail.com>
2024-08-16 20:48:53 +0900

    feat: 画像生成時にローディングを追加する

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,31 @@ import styles from './page.module.css';
 import MonsterForm from './components/MonsterForm';
 import fetchMonsterImg from './lib/getImgApi';
 import Image from 'next/image';
+import Loading from 'react-loading';
 
 export default function Home() {
   const [formData, setFormData] = useState({ description: '', attribute: '' });
   const [monsterImg, setMonsterImg] = useState('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleFormSubmit = async (description: string, attribute: string) => {
+    setIsLoading(true);
     setFormData({ description, attribute });
     const imageUrl: string = await fetchMonsterImg({ description, attribute });
     setMonsterImg(imageUrl);
+    setIsLoading(false);
   };
+
+  const LoadingSection = () => (
+    <>
+      <Loading
+        type="spinningBubbles"
+        color="#0070f3"
+        height={100}
+        width={100}
+      />
+    </>
+  );
 
   return (
     <div className={styles.container}>
@@ -21,16 +36,20 @@ export default function Home() {
         <h1 className={styles.title}>Ai Monster Generator</h1>
         <MonsterForm onSubmit={handleFormSubmit} />
         <div className={styles.imageContainer}>
-          {monsterImg && (
-            <>
-              <Image
-                src={monsterImg}
-                alt="生成されたモンスターの画像"
-                className={styles.monsterImage}
-                width={300}
-                height={300}
-              />
-            </>
+          {isLoading ? (
+            <LoadingSection />
+          ) : (
+            monsterImg && (
+              <>
+                <Image
+                  src={monsterImg}
+                  alt="生成されたモンスターの画像"
+                  className={styles.monsterImage}
+                  width={300}
+                  height={300}
+                />
+              </>
+            )
           )}
         </div>
       </main>
