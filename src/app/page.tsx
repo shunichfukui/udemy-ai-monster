70f4cfb810840280766bb3e745cb13419900b4f2
shunich <fukusyun3@gmail.com>
2024-08-16 10:27:28 +0900

    feat: inputフォームの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+'use client';
+import { useState } from 'react';
 import styles from './page.module.css';
+import MonsterForm from './components/MonsterForm';
 
 export default function Home() {
+  const [formData, setFormData] = useState({ description: '', attribute: '' });
+
+  const handleFormSubmit = (description: string, attribute: string) => {
+    setFormData({ description, attribute });
+  };
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>Ai Monster Generator</h1>
+        <MonsterForm onSubmit={handleFormSubmit} />
       </main>
     </div>
   );
