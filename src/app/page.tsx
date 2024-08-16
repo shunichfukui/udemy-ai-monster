9ca1ac656f7a4997b4c612e41db6167bedf20878
shunich <fukusyun3@gmail.com>
2024-08-16 20:29:45 +0900

    feat: フォーム送信時に画像を生成し表示する

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,17 @@
 import { useState } from 'react';
 import styles from './page.module.css';
 import MonsterForm from './components/MonsterForm';
+import fetchMonsterImg from './lib/getImgApi';
+import Image from 'next/image';
 
 export default function Home() {
   const [formData, setFormData] = useState({ description: '', attribute: '' });
+  const [monsterImg, setMonsterImg] = useState('');
 
-  const handleFormSubmit = (description: string, attribute: string) => {
+  const handleFormSubmit = async (description: string, attribute: string) => {
     setFormData({ description, attribute });
+    const imageUrl: string = await fetchMonsterImg({ description, attribute });
+    setMonsterImg(imageUrl);
   };
 
   return (
@@ -15,6 +20,19 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Ai Monster Generator</h1>
         <MonsterForm onSubmit={handleFormSubmit} />
+        <div className={styles.imageContainer}>
+          {monsterImg && (
+            <>
+              <Image
+                src={monsterImg}
+                alt="生成されたモンスターの画像"
+                className={styles.monsterImage}
+                width={300}
+                height={300}
+              />
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
